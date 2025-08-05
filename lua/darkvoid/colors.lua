3e98141223ff0c82682de6fd73b803faa9bd2dd6
pantosaur <pantosaur@gmail.com>
2025-08-05 14:54:09 -0400

    use background to colors.searchHighlight

--- lua/darkvoid/colors.lua
+++ lua/darkvoid/colors.lua
@@ -97,7 +97,7 @@ function M.setup(user_config)
 		Constant = { fg = colors.constant },
 
 		Search = { fg = colors.search_highlight, bg = "NONE", },
-		IncSearch = { fg = colors.search_highlight, bg = "NONE", gui = "bold" },
+		IncSearch = { fg = colors.fg, bg = colors.search_highlight, gui = "bold" },
 		Operator = { fg = colors.operator },
 		Delimiter = { fg = colors.bracket },
 
